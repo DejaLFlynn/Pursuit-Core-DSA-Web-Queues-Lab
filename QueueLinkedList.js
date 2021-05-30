e73f959add4fe63ca8b97d36064f92596f4875f5
DejaLFlynn <dejaflynn@pursuit.org>
2021-05-30 19:00:00 -0400

    queue linked list

--- QueueLinkedList.js
+++ QueueLinkedList.js
@@ -10,13 +10,51 @@ class Node {
 }
 
 
+
+
 /*
   QueueLinkedList will be a linked list with head, tail and length and it will have
   the Queue API methods: .enqueue(), .dequeue(), .peek() and .isEmpty() etc.
 */
 class QueueLinkedList {
-   
+  enqueue(val){
+    let newNode = new Node(val);
+    if(!this.value){
+      this.value = newNode
+      this.next = newNode;
+    }else{
+      this.next.next = newNode;
+      this.next = newNode;
+    }
 
+  }
+  dequeue(){
+    if(!this.value) return null
+    const temp = this.value
+    if(this.value===this.next){
+      this.next = null
+    }
+    this.value = this.value.next;
+    return temp.val
+  }
+
+  peek(){
+    this.value = function(){
+      if(head){
+        return head.value
+      }
+    }
+    return null
+  }
+   
+  isEmpty(){
+    this.val = function(){
+      if(this.value.length == 0){
+
+      }
+    }
+    return length === 0
+  }
 
 }
 
